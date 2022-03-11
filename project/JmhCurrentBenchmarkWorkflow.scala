9f88a182de247acb8fdd71ccc8ce660634f9de06
shrutiverma97 <shruti@dream11.com>
2022-03-11 15:40:46 +0530

    wip

--- project/JmhCurrentBenchmarkWorkflow.scala
+++ project/JmhCurrentBenchmarkWorkflow.scala
@@ -18,26 +18,35 @@ object JmhCurrentBenchmarkWorkflow {
     s"""sbt -no-colors -v "zhttpBenchmarks/jmh:run -i 3 -wi 3 -f1 -t1 $str" | grep "thrpt" >> ../${list.head}.txt""".stripMargin)
 
   def groupedBenchmarks(batchSize: Int) = getFilenames.grouped(batchSize).toList
-  def dependencies(batchSize: Int, branch: String) = groupedBenchmarks(batchSize).flatMap((l: Seq[String]) => List(s"run_jmh_benchmark_${branch}_${l.head}"))
+  def dependencies(batchSize: Int) = groupedBenchmarks(batchSize).flatMap((l: Seq[String]) => List(s"run_jmh_benchmark_Current_${l.head}",s"run_jmh_benchmark_Main_${l.head}"))
 
-  def jmhPublish(batchSize: Int, branch: String = "Current") = Seq(WorkflowJob(
-  id = s"jmh_publish_${branch}",
-  name = s"Jmh Publish ${branch}",
+  def jmhPublish(batchSize: Int) = Seq(WorkflowJob(
+  id = "jmh_publish",
+  name = "Jmh Publish",
   scalas = List(Scala213),
-  needs =  dependencies(batchSize, branch),
-  steps = groupedBenchmarks(batchSize).map(l => {
+  needs =  dependencies(batchSize),
+  steps = groupedBenchmarks(batchSize).flatMap(l => {
+    Seq(
     WorkflowStep.Use(
       ref = UseRef.Public("actions", "download-artifact", "v3"),
       Map(
-        "name" -> s"jmh_result_${l.head}"
+        "name" -> s"jmh_result_Current_${l.head}"
       )
+    ),
+    WorkflowStep.Use(
+      ref = UseRef.Public("actions", "download-artifact", "v3"),
+      Map(
+        "name" -> s"jmh_result_Main_${l.head}"
+      )
+    )
     )
   }) ++ Seq(
     WorkflowStep.Run(
       commands = List(
-        """rm -f body.txt
-        |cat > body.txt
-        |echo "::set-output name=res::$(echo "$(<body.txt)")"
+        """rm -f body1.txt
+        |cat > body2.txt
+        |rm -f body2.txt
+        |cat > body2.txt
         |""".stripMargin),
       id = Some("create_comment"),
       name = Some("Create Comment")
@@ -48,20 +57,31 @@ object JmhCurrentBenchmarkWorkflow {
         s"""while IFS= read -r line; do
            |   IFS=' ' read -ra PARSED_RESULT <<< "$$line"
            |   B_VALUE=$$(echo "$${PARSED_RESULT[1]}": "$${PARSED_RESULT[4]}" ops/sec"")
-           |   echo $$B_VALUE >> body.txt
-           | done < ${l.head}.txt""".stripMargin),
-      id = Some(s"result_${l.head}"),
-      name = Some(s"Result ${l.head}")
+           |   echo $$B_VALUE >> body1.txt
+           | done < Current_${l.head}.txt
+           |while IFS= read -r line; do
+           |   IFS=' ' read -ra PARSED_RESULT <<< "$$line"
+           |   B_VALUE=$$(echo "$${PARSED_RESULT[1]}": "$${PARSED_RESULT[4]}" ops/sec"")
+           |   echo $$B_VALUE >> body2.txt
+           | done < Main_${l.head}.txt """.stripMargin),
+      id = Some(s"result_current_${l.head}"),
+      name = Some(s"Result Current ${l.head}")
     )
   }) ++ Seq(
     WorkflowStep.Run(
       commands = List(
-      """body=$(cat body.txt)
-        | body="${body//'%'/'%25'}"
-        | body="${body//$'\n'/'%0A'}"
-        | body="${body//$'\r'/'%0D'}"
-        | echo "$body"
-        | echo "::set-output name=body::$(echo "$body")"""".stripMargin
+      """body1=$(cat body1.txt)
+        | body1="${body1//'%'/'%25'}"
+        | body1="${body1//$'\n'/'%0A'}"
+        | body1="${body1//$'\r'/'%0D'}"
+        | echo "$body1"
+        | echo "::set-output name=body1::$(echo "$body1")
+        | body2=$(cat body2.txt)
+        | body2="${body2//'%'/'%25'}"
+        | body2="${body2//$'\n'/'%0A'}"
+        | body2="${body2//$'\r'/'%0D'}"
+        | echo "$body2"
+        | echo "::set-output name=body2::$(echo "$body2")"""".stripMargin
       ),
       id = Some("set_output"),
       name = Some("Set Output")
@@ -75,21 +95,25 @@ object JmhCurrentBenchmarkWorkflow {
             |**\uD83D\uDE80 Jmh Benchmark:**
             |
             |- **Current Branch**:
-            | ${{steps.set_output.outputs.body}}""".stripMargin
+            | ${{steps.set_output.outputs.body1}}
+            |
+            | - **Main Branch**:
+            | ${{steps.set_output.outputs.body2}}""".stripMargin
       )
     )
   )
 )
   )
 
-  def jmhRun(batchSize: Int, branch: String = "Current") = groupedBenchmarks(batchSize).map(l =>
+  def jmhRun(batchSize: Int, branch: String = "Current") = groupedBenchmarks(batchSize).map(l => {
+    val checkout = if(branch == "Current") "v2" else "main"
     WorkflowJob(
       id = s"run_jmh_benchmark_${branch}_${l.head}",
       name = s"Jmh Benchmark ${branch} ${l.head}",
       scalas = List(Scala213),
       steps = List(
         WorkflowStep.Use(
-          UseRef.Public("actions", "checkout", "v2"),
+          UseRef.Public("actions", "checkout", s"$checkout"),
           Map(
             "path" -> "zio-http"
           )
@@ -103,21 +127,22 @@ object JmhCurrentBenchmarkWorkflow {
         ),
         WorkflowStep.Run(
           env = Map("GITHUB_TOKEN" -> "${{secrets.ACTIONS_PAT}}"),
-          commands = List("cd zio-http", s"sed -i -e '$$a${jmhPlugin}' project/plugins.sbt", s"rm -f ${l.head}.txt",s"cat > ${l.head}.txt") ++ sbtCommand(l),
+          commands = List("cd zio-http", s"sed -i -e '$$a${jmhPlugin}' project/plugins.sbt", s"rm -f ${l.head}.txt", s"cat > ${l.head}.txt") ++ sbtCommand(l),
           id = Some("run_benchmark"),
           name = Some("Run Benchmark")
         ),
         WorkflowStep.Use(
           UseRef.Public("actions", "upload-artifact", s"v3"),
           Map(
-            "name" -> s"jmh_result_${l.head}",
-            "path" -> s"${l.head}.txt"
+            "name" -> s"jmh_result_${branch}_${l.head}",
+            "path" -> s"${branch}_${l.head}.txt"
           )
         )
       )
     )
+  }
   )
 
-  def apply(batchSize: Int): Seq[WorkflowJob] = jmhRun(batchSize) ++ jmhPublish(batchSize) ++ jmhRun(batchSize, "Main") ++ jmhPublish(batchSize, "Main")
+  def apply(batchSize: Int): Seq[WorkflowJob] = jmhRun(batchSize) ++ jmhRun(batchSize, "Main") ++ jmhPublish(batchSize)
 
 }
